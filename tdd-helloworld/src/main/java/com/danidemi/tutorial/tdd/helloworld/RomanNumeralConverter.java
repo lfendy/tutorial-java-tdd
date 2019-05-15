828cbd4124ca1d11da2248552bf3f187c9bbe0c7
name <a@a.a>
2019-05-15 20:12:32 +1000

    finished adding all multiples of singles and tests

--- tdd-helloworld/src/main/java/com/danidemi/tutorial/tdd/helloworld/RomanNumeralConverter.java
+++ tdd-helloworld/src/main/java/com/danidemi/tutorial/tdd/helloworld/RomanNumeralConverter.java
@@ -3,7 +3,29 @@ package com.danidemi.tutorial.tdd.helloworld;
 public class RomanNumeralConverter {
 
   public int convertRoman(String roman){
-    return roman.length();
+    return multiplesOfSingles(roman);
   }
+
+  public int multiplesOfSingles(String roman){
+    return roman.length() * translateFirstRoman(roman);
+  }
+
+  public int translateFirstRoman(String roman){
+    String firstRoman = roman.substring(0, 1);
+
+    switch(firstRoman){
+      case "I":
+        return 1;
+      case "X":
+        return 10;
+      case "C":
+        return 100;
+      case "M":
+        return 1000;
+      default:
+        return 0;
+    }
+  }
+
 
 }
--- tdd-helloworld/src/test/java/com/danidemi/tutorial/tdd/helloworld/RomanNumeralConverterTest.java
+++ tdd-helloworld/src/test/java/com/danidemi/tutorial/tdd/helloworld/RomanNumeralConverterTest.java
@@ -10,14 +10,46 @@ public class RomanNumeralConverterTest {
 
   @Test
   public void shouldReturnMultiplesOfSingles(){
-    String[] romans = new String[] {"I", "II", "III"};
-    int[] results = new int[] {1,2,3};
+    String[] romans = new String[] {
+      "I",
+      "II",
+      "III",
+      "X",
+      "XX",
+      "XXX",
+      "C",
+      "CC",
+      "CCC",
+      "M",
+      "MM",
+      "MMM",
+    };
+    int[] results = new int[] {
+      1,
+      2,
+      3,
+      10,
+      20,
+      30,
+      100,
+      200,
+      300,
+      1000,
+      2000,
+      3000,
+    };
 
+    validateRomans(romans, results);
+
+  }
+
+  private void validateRomans(String[] romans, int[] results){
     for(int i = 0; i < romans.length; i++){
       String roman = romans[i];
       int result = results[i];
       validateRoman(roman, result);
     }
+
   }
 
   private void validateRoman(String roman, int givenResult){
